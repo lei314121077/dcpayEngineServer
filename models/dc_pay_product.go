06fb34d3d10e9be6a17b6ef1826cb7f758a09061
lei314121077 <ldx314121077@163.com>
2016-11-18 17:01:19 +0800

    新增支付产品模型

--- models/dc_pay_product.go
+++ models/dc_pay_product.go
@@ -1,1 +1,25 @@
-dc_pay_product.go
+package main
+
+import (
+	"fmt"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+/**
+**支付产品
+**/
+type DcPayProduct struct {
+   id                   string 'orm:"size(50)"'	
+   create_time          time.Time 'orm:"auto_now_add;type(datetime)"'				//创建时间
+   edit_time            time.Time 'orm:"auto_now_add;type(datetime)"'				//修改时间
+   version              int64														//版本编号
+   status               string 'orm:"size(36)"'										//状态码
+   product_code         string 'orm:"size(50)"'										//支付产品编号
+   product_name         string 'orm:"size(200)"'									//支付产品名称
+   audit_status         string 'orm:"size(45)"'										//审计状态码
+}
+
+func (u *DcPayProduct) TableName() string {
+    return "dc_pay_product_tb"   //支付产品表
+}
--- models/dc_pay_way.go
+++ models/dc_pay_way.go
@@ -17,11 +17,11 @@ type DcPayWay struct{
    pay_way_code         string 'orm:"size(50)"'									  //支付方式编号
    pay_way_name         string 'orm:"size(100)"'    							  //支付方式名称
    pay_type_code        string 'orm:"size(50)"'									  //支付类型编号
-   pay_type_name        string 'orm:"size(100)"'                                  //支付类型名称
+   pay_type_name        string 'orm:"size(100)"'								  //支付类型名称
    pay_product_code     string 'orm:"size(50)"'									  //支付产品编号
    status               string 'orm:"size(36)"' 								  //状态(100:正常状态,101非正常状态)
    sorts                int64 default 1000 										  //排序(倒序排序,默认值1000)
-   pay_rate             float64 												 //商户支付费率
+   pay_rate             float64 												  //商户支付费率
 }
 
 func (u *DcPayWay) TableName() string {
